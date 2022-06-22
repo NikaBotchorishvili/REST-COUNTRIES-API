059c9fd052ae27f57b67dbaefe1e4359e4b6b13d
NikaBotchorishvili <nikabotchorishvili@gmail.com>
2022-06-22 22:49:28 +0400

    this fixes #2

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ Thanks for checking out this front-end coding challenge.
 **To do this challenge, you need a good understanding of HTML, CSS, and JavaScript.**
 
 ## The challenge
-
-Your challenge is to integrate with the [REST Countries API](https://restcountries.com) to pull country data and display it like in the designs.
+h
+Your challenge is to integrate with the [REST Countries API](ttps://restcountries.com) to pull country data and display it like in the designs.
 
 You can use any JavaScript framework/library on the front-end such as [React](https://reactjs.org) or [Vue](https://vuejs.org). You also have complete control over which packages you use to do things like make HTTP requests or style your project.
 
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,36 @@
+"use strict";
 const xhr = new XMLHttpRequest();
+
+let darkModeCounter = 0;
 
 const countriesEl = document.getElementById("countries");
 const paginationEl = document.getElementById("pagination");
+const body = document.getElementsByTagName("body")[0];
 let current_page = 1;
 let rows = 20;
 
+const regionValues = ["Africa", "America", "Asia", "Australia", "Europe"];
+
+const FilterByRegion = () => {
+	const filterInput = document.getElementById("region").value;
+
+	if (regionValues.includes(filterInput)) {
+		xhr.open("GET", `https://restcountries.com/v3.1/region/${filterInput}`);
+	} else {
+		xhr.open("GET", "https://restcountries.com/v3.1/all", true);
+	}
+	xhr.send();
+};
+
 xhr.open("GET", "https://restcountries.com/v3.1/all", true);
+
 xhr.send();
 
 xhr.onload = () => {
 	if (xhr.status === 200) {
 		let json = JSON.parse(xhr.response);
-
 		DisplayCountries(json, rows, current_page);
-
+		SetupPagination(json, paginationEl, rows);
 	} else {
 		console.log(`Error: ${xhr.status}`);
 	}
@@ -25,15 +42,15 @@ const DisplayCountries = (data, rows_per_page, page) => {
 
 	let start = rows_per_page * page;
 	let end = start + rows_per_page;
-	let paginated_items = data.slice(start, end);
 
-	for (let i = start; i < paginated_items.length; i++) {
+	for (let i = start; i < end; i++) {
 		let card = document.createElement("div");
 		let countryFlagEl = document.createElement("img");
 		let countryNameEl = document.createElement("h1");
 		let countryPopulationEl = document.createElement("h5");
 		let countryRegionEl = document.createElement("h5");
 		let countryCapitalEl = document.createElement("h5");
+		let countryInfoEl = document.createElement("div");
 
 		let countryImageSvg = data[i].flags.png;
 		let countryName = data[i].name.common;
@@ -46,8 +63,10 @@ const DisplayCountries = (data, rows_per_page, page) => {
 		countryPopulationEl.classList.add("population");
 		countryRegionEl.classList.add("region");
 		countryCapitalEl.classList.add("capital");
+		countryInfoEl.classList.add("country_info");
 
 		countryFlagEl.src = countryImageSvg;
+
 		countryNameEl.innerText = countryName;
 		countryPopulationEl.innerHTML = "Population: " + countryPopulation;
 		countryRegionEl.innerHTML = "Region: " + countryRegion;
@@ -63,3 +82,62 @@ const DisplayCountries = (data, rows_per_page, page) => {
 	}
 };
 
+const SetupPagination = (countries, wrapper, rows_per_page) => {
+	wrapper.innerHTML = "";
+
+	let page_count = Math.ceil(countries.length / rows_per_page);
+
+	for (let i = 1; i < page_count + 1; i++) {
+		let btn = PaginationButton(i, countries);
+
+		wrapper.appendChild(btn);
+	}
+};
+
+const PaginationButton = (page, items) => {
+	let button = document.createElement("button");
+	button.classList.add("p-btn");
+	button.innerText = page;
+
+	if (current_page == page) button.classList.add("active");
+
+	button.addEventListener("click", () => {
+		current_page = page;
+
+		DisplayCountries(items, rows, page);
+
+		let currentBtn = document.querySelector("#pagination button.active");
+		currentBtn.classList.remove("active");
+
+		button.classList.add("active");
+	});
+	return button;
+};
+
+const DarkMode = () => {
+	const header = document.getElementById("header");
+	const darkModeBtn = document.getElementById("dark-mode-btn");
+	const darkModeImage = document.getElementById("dm-img");
+	const cards = document.querySelectorAll(".card");
+	const inputs = document.querySelectorAll(".inputs");
+	const paginationBtns = document.querySelectorAll(".p-btn");
+
+	paginationBtns.forEach((btn) => {
+		btn.classList.toggle("dark-p-btn");
+	});
+	cards.forEach((card) => {
+		card.classList.toggle("dark-mode-bg");
+	});
+
+	inputs.forEach((e) => {
+		e.classList.toggle("dark-mode-bg");
+	});
+
+	body.classList.toggle("dark-bg");
+
+	header.classList.toggle("dark-mode-bg");
+	darkModeBtn.classList.toggle("dark-btn");
+	darkModeImage.classList.toggle("white-dark-mode-img");
+
+	darkModeCounter++;
+};
