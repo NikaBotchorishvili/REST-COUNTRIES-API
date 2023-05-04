604f7a0826887365f99ad001c3c59de02fe119fa
NikaBotchorishvili <nikabotchorishvili@gmail.com>
2023-05-04 21:17:46 +0400

    revamped js to ts, changed from XMLHTPPRQUEST to fetch and simplified the code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,143 +1,113 @@
 "use strict";
-const xhr = new XMLHttpRequest();
-
-let darkModeCounter = 0;
-
-const countriesEl = document.getElementById("countries");
-const paginationEl = document.getElementById("pagination");
-const body = document.getElementsByTagName("body")[0];
+const countriesEl = document.querySelector("#countries");
+const paginationEl = document.querySelector("#pagination");
+const body = document.querySelector("body");
 let current_page = 1;
 let rows = 20;
-
+let darkMode = false;
+let data = [];
 const regionValues = ["Africa", "America", "Asia", "Australia", "Europe"];
-
-const FilterByRegion = () => {
-	const filterInput = document.getElementById("region").value;
-
-	if (regionValues.includes(filterInput)) {
-		xhr.open("GET", `https://restcountries.com/v3.1/region/${filterInput}`);
-	} else {
-		xhr.open("GET", "https://restcountries.com/v3.1/all", true);
-	}
-	xhr.send();
+const init = () => {
+    const filterInputElement = document.getElementById("region");
+    let filterInputValue = filterInputElement.value;
+    fetch(`https://restcountries.com/v3.1/${filterInputValue == "all" ? "all" : `region/${filterInputValue}`}`)
+        .then((response) => response.json())
+        .then((data) => {
+        DisplayCountries(data, rows, current_page);
+        SetupPagination(data, paginationEl, rows);
+    })
+        .catch((error) => {
+        console.log(`Error: ${error}`);
+    });
 };
-
-xhr.open("GET", "https://restcountries.com/v3.1/all", true);
-
-xhr.send();
-
-xhr.onload = () => {
-	if (xhr.status === 200) {
-		let json = JSON.parse(xhr.response);
-		DisplayCountries(json, rows, current_page);
-		SetupPagination(json, paginationEl, rows);
-	} else {
-		console.log(`Error: ${xhr.status}`);
-	}
-};
-
+init();
 const DisplayCountries = (data, rows_per_page, page) => {
-	countriesEl.innerHTML = "";
-	page--;
-
-	let start = rows_per_page * page;
-	let end = start + rows_per_page;
-
-	for (let i = start; i < end; i++) {
-		let card = document.createElement("div");
-		let countryFlagEl = document.createElement("img");
-		let countryNameEl = document.createElement("h1");
-		let countryPopulationEl = document.createElement("h5");
-		let countryRegionEl = document.createElement("h5");
-		let countryCapitalEl = document.createElement("h5");
-		let countryInfoEl = document.createElement("div");
-
-		let countryImageSvg = data[i].flags.png;
-		let countryName = data[i].name.common;
-		let countryCapital = data[i].capital;
-		let countryPopulation = data[i].population;
-		let countryRegion = data[i].region;
-
-		card.classList.add("card");
-		countryNameEl.classList.add("country_name");
-		countryPopulationEl.classList.add("population");
-		countryRegionEl.classList.add("region");
-		countryCapitalEl.classList.add("capital");
-		countryInfoEl.classList.add("country_info");
-
-		countryFlagEl.src = countryImageSvg;
-
-		countryNameEl.innerText = countryName;
-		countryPopulationEl.innerHTML = "Population: " + countryPopulation;
-		countryRegionEl.innerHTML = "Region: " + countryRegion;
-		countryCapitalEl.innerText = "Capital: " + countryCapital;
-
-		card.appendChild(countryFlagEl);
-		card.appendChild(countryNameEl);
-		card.appendChild(countryPopulationEl);
-		card.appendChild(countryRegionEl);
-		card.appendChild(countryCapitalEl);
-
-		countriesEl.appendChild(card);
-	}
+    countriesEl.innerHTML = "";
+    page--;
+    let start = rows_per_page * page;
+    let end = start + rows_per_page;
+    for (let i = start; i < end; i++) {
+        let card = document.createElement("div");
+        let countryFlagEl = document.createElement("img");
+        let countryNameEl = document.createElement("a");
+        let countryPopulationEl = document.createElement("h5");
+        let countryRegionEl = document.createElement("h5");
+        let countryCapitalEl = document.createElement("h5");
+        let countryInfoEl = document.createElement("div");
+        let countryImage = data[i].flags.png;
+        let countryName = data[i].name.common;
+        let countryCapital = data[i].capital;
+        let countryPopulation = data[i].population;
+        let countryRegion = data[i].region;
+        card.classList.add("card");
+        countryNameEl.classList.add("country_name");
+        countryNameEl.href = `item.html?id=${i}`;
+        countryNameEl.classList.add("white");
+        countryPopulationEl.classList.add("population");
+        countryRegionEl.classList.add("region");
+        countryCapitalEl.classList.add("capital");
+        countryInfoEl.classList.add("country_info");
+        countryFlagEl.src = countryImage;
+        countryNameEl.innerText = countryName;
+        countryPopulationEl.innerHTML = "Population: " + countryPopulation;
+        countryRegionEl.innerHTML = "Region: " + countryRegion;
+        countryCapitalEl.innerText = "Capital: " + countryCapital;
+        countryInfoEl.append(countryNameEl, countryPopulationEl, countryRegionEl, countryCapitalEl);
+        card.append(countryFlagEl, countryInfoEl);
+        DarkMode();
+        countriesEl.appendChild(card);
+    }
 };
-
 const SetupPagination = (countries, wrapper, rows_per_page) => {
-	wrapper.innerHTML = "";
-
-	let page_count = Math.ceil(countries.length / rows_per_page);
-
-	for (let i = 1; i < page_count + 1; i++) {
-		let btn = PaginationButton(i, countries);
-
-		wrapper.appendChild(btn);
-	}
+    wrapper.innerHTML = "";
+    let page_count = Math.ceil(countries.length / rows_per_page);
+    for (let i = 1; i < page_count + 1; i++) {
+        let btn = PaginationButton(i, countries);
+        wrapper.appendChild(btn);
+    }
 };
-
 const PaginationButton = (page, items) => {
-	let button = document.createElement("button");
-	button.classList.add("p-btn");
-	button.innerText = page;
-
-	if (current_page == page) button.classList.add("active");
-
-	button.addEventListener("click", () => {
-		current_page = page;
-
-		DisplayCountries(items, rows, page);
-
-		let currentBtn = document.querySelector("#pagination button.active");
-		currentBtn.classList.remove("active");
-
-		button.classList.add("active");
-	});
-	return button;
+    let button = document.createElement("button");
+    button.classList.add("p-btn");
+    button.innerText += page;
+    if (current_page == page)
+        button.classList.add("active");
+    button.addEventListener("click", () => {
+        current_page = page;
+        DisplayCountries(items, rows, page);
+        let currentBtn = document.querySelector("#pagination button.active");
+        currentBtn.classList.remove("active");
+        button.classList.add("active");
+    });
+    return button;
 };
-
 const DarkMode = () => {
-	const header = document.getElementById("header");
-	const darkModeBtn = document.getElementById("dark-mode-btn");
-	const darkModeImage = document.getElementById("dm-img");
-	const cards = document.querySelectorAll(".card");
-	const inputs = document.querySelectorAll(".inputs");
-	const paginationBtns = document.querySelectorAll(".p-btn");
-
-	paginationBtns.forEach((btn) => {
-		btn.classList.toggle("dark-p-btn");
-	});
-	cards.forEach((card) => {
-		card.classList.toggle("dark-mode-bg");
-	});
-
-	inputs.forEach((e) => {
-		e.classList.toggle("dark-mode-bg");
-	});
-
-	body.classList.toggle("dark-bg");
-
-	header.classList.toggle("dark-mode-bg");
-	darkModeBtn.classList.toggle("dark-btn");
-	darkModeImage.classList.toggle("white-dark-mode-img");
-
-	darkModeCounter++;
+    const header = document.querySelector(".header");
+    const colorSchemeBtn = document.querySelector(".color-scheme-btn");
+    const colorSchemeModeImage = document.querySelector(".color-scheme-image");
+    const cards = document.querySelectorAll(".card");
+    const inputs = document.querySelectorAll(".inputs");
+    const paginationBtns = document.querySelectorAll(".p-btn");
+    paginationBtns.forEach((btn) => {
+        btn.classList.toggle("dark-p-btn");
+    });
+    cards.forEach((card) => {
+        let countryName = card.querySelector(".country_name");
+        if (darkMode) {
+            card.classList.remove("dark-mode-bg");
+            countryName.classList.remove("white");
+        }
+        else {
+            card.classList.add("dark-mode-bg");
+            countryName.classList.add("white");
+        }
+    });
+    inputs.forEach((e) => {
+        e.classList.toggle("dark-mode-bg");
+    });
+    body.classList.toggle("dark-bg");
+    header.classList.toggle("dark-mode-bg");
+    colorSchemeBtn.classList.toggle("dark-btn");
+    colorSchemeModeImage.classList.toggle("white-dark-mode-img");
+    darkMode = !darkMode;
 };
---
+++ assets/js/index.ts
@@ -0,0 +1,191 @@
+type data = {
+	altSpellings: string[];
+	area: number;
+	borders: string[];
+	capital: string[];
+	capitalInfo: { latlng: number[] };
+	car: { side: string; signs: string[] };
+	cca2: string;
+	cca3: string;
+	ccn3: string;
+	cioc: string;
+	coatOfArms: { png: string; svg: string };
+	continents: string[];
+	fifa: string;
+	flag: string;
+	flags: { png: string; svg: string; alt: string };
+	independent: boolean;
+	landlocked: boolean;
+	name: { common: string; official: string };
+	population: number;
+	region: string;
+	startOfWeek: string;
+	status: string;
+	subregion: string;
+	timezones: string[];
+	tld: string[];
+	unMember: boolean;
+};
+
+
+const countriesEl = document.querySelector("#countries") as HTMLElement;
+const paginationEl = document.querySelector("#pagination") as HTMLElement; 
+const body = document.querySelector("body") as HTMLElement;
+const regionInputElement = document.querySelector("#region") as HTMLSelectElement;
+
+
+let current_page = 1;
+let rows = 20;
+
+let darkMode = false;
+let data: data[] = [];
+const regionValues = ["Africa", "America", "Asia", "Australia", "Europe"];
+
+regionInputElement.addEventListener("change", () => {
+	init();
+})
+const init = () => {
+	const filterInputElement = document.getElementById(
+		"region"
+	) as HTMLInputElement;
+	let filterInputValue = filterInputElement.value;
+
+	fetch(
+		`https://restcountries.com/v3.1/${
+			filterInputValue == "all" ? "all" : `region/${filterInputValue}`
+		}`
+	)
+		.then((response) => response.json())
+		.then((data) => {
+			DisplayCountries(data, rows, current_page);
+			SetupPagination(data, paginationEl, rows);
+		})
+		.catch((error) => {
+			console.log(`Error: ${error}`);
+		});
+};
+
+init();
+
+const DisplayCountries = (data: data[], rows_per_page: number, page: number) => {
+	countriesEl.innerHTML = "";
+	page--;
+
+	let start = rows_per_page * page;
+	let end = start + rows_per_page;
+
+	for (let i = start; i < end; i++) {
+		let card = document.createElement("div");
+		let countryFlagEl = document.createElement("img");
+		let countryNameEl = document.createElement("a");
+		let countryPopulationEl = document.createElement("h5");
+		let countryRegionEl = document.createElement("h5");
+		let countryCapitalEl = document.createElement("h5");
+		let countryInfoEl = document.createElement("div");
+
+		let countryImage: string = data[i].flags.png;
+		let countryName = data[i].name.common;
+		let countryCapital = data[i].capital;
+		let countryPopulation = data[i].population;
+		let countryRegion = data[i].region;
+
+		card.classList.add("card");
+		countryNameEl.classList.add("country_name");
+
+		countryNameEl.href = `item.html?id=${i}`;
+		countryNameEl.classList.add("white")
+		countryPopulationEl.classList.add("population");
+		countryRegionEl.classList.add("region");
+		countryCapitalEl.classList.add("capital");
+		countryInfoEl.classList.add("country_info");
+
+		countryFlagEl.src = countryImage;
+
+		countryNameEl.innerText = countryName;
+		countryPopulationEl.innerHTML = "Population: " + countryPopulation;
+		countryRegionEl.innerHTML = "Region: " + countryRegion;
+		countryCapitalEl.innerText = "Capital: " + countryCapital;
+
+		countryInfoEl.append(
+			countryNameEl,
+			countryPopulationEl,
+			countryRegionEl,
+			countryCapitalEl
+		);
+		card.append(countryFlagEl, countryInfoEl);
+		DarkMode()
+		countriesEl.appendChild(card);
+	}
+};
+
+const SetupPagination = (countries: data[], wrapper: HTMLElement, rows_per_page: number) => {
+	wrapper.innerHTML = "";
+	let page_count = Math.ceil(countries.length / rows_per_page);
+
+	for (let i = 1; i < page_count + 1; i++) {
+		let btn = PaginationButton(i, countries);
+
+		wrapper.appendChild(btn);
+	}
+};
+
+const PaginationButton = (page: number, items: data[]) => {
+	let button = document.createElement("button") as HTMLButtonElement;
+	button.classList.add("p-btn");
+	button.innerText += page;
+
+	if (current_page == page) button.classList.add("active");
+
+	button.addEventListener("click", () => {
+		current_page = page;
+
+		DisplayCountries(items, rows, page);
+
+		let currentBtn = document.querySelector(
+			"#pagination button.active"
+		) as HTMLButtonElement;
+		currentBtn.classList.remove("active");
+
+		button.classList.add("active");
+	});
+	return button;
+};
+
+const DarkMode = () => {
+	const header = document.querySelector(".header") as HTMLElement;
+	const colorSchemeBtn = document.querySelector(
+		".color-scheme-btn"
+	) as HTMLElement;
+	const colorSchemeModeImage = document.querySelector(
+		".color-scheme-image"
+	) as HTMLElement;
+	const cards = document.querySelectorAll(".card");
+	const inputs = document.querySelectorAll(".inputs");
+	const paginationBtns = document.querySelectorAll(".p-btn");
+
+	paginationBtns.forEach((btn) => {
+		btn.classList.toggle("dark-p-btn");
+	});
+	cards.forEach((card) => {
+		let countryName = card.querySelector(".country_name") as HTMLElement
+		if(darkMode){
+			card.classList.remove("dark-mode-bg");
+			countryName.classList.remove("white")
+		}else{
+			card.classList.add("dark-mode-bg");
+			countryName.classList.add("white")
+		}
+
+	});
+
+	inputs.forEach((e) => {
+		e.classList.toggle("dark-mode-bg");
+	});
+
+	body.classList.toggle("dark-bg");
+
+	header.classList.toggle("dark-mode-bg");
+	colorSchemeBtn.classList.toggle("dark-btn");
+	colorSchemeModeImage.classList.toggle("white-dark-mode-img");
+	darkMode = !darkMode;
+};
